508a8ebfd2cc047d0649d6100cc0e25a62775158
Tamerlan Beimzhan <tbeimzhan@gmail.com>
2023-05-25 14:49:15 +0600

    Refactor code

--- nfactorial_2048.py
+++ nfactorial_2048.py
@@ -2,48 +2,67 @@ import curses
 import random
 import sys
 
+LEVEL_NAMES = [
+    "Master - 3x3",
+    "Normal - 4x4",
+    "Easy - 5x5",
+    "Relax - 6x6"
+]
+LEVELS = [(3, 3), (4, 4), (5, 5), (6, 6)]
+
+COLOR_PAIRS = {
+    1: (curses.COLOR_YELLOW, curses.COLOR_BLACK),
+    2: (curses.COLOR_WHITE, curses.COLOR_BLUE),
+    3: (curses.COLOR_WHITE, curses.COLOR_GREEN),
+    4: (curses.COLOR_WHITE, curses.COLOR_MAGENTA),
+    5: (curses.COLOR_WHITE, curses.COLOR_CYAN),
+    6: (curses.COLOR_WHITE, curses.COLOR_RED),
+    7: (curses.COLOR_WHITE, curses.COLOR_YELLOW),
+    8: (curses.COLOR_BLACK, curses.COLOR_WHITE),
+    9: (curses.COLOR_BLACK, curses.COLOR_BLUE),
+    10: (curses.COLOR_BLACK, curses.COLOR_GREEN),
+    11: (curses.COLOR_BLACK, curses.COLOR_MAGENTA),
+    12: (curses.COLOR_BLACK, curses.COLOR_CYAN),
+    13: (curses.COLOR_BLACK, curses.COLOR_RED),
+    14: (curses.COLOR_BLACK, curses.COLOR_YELLOW),
+    15: (curses.COLOR_WHITE, curses.COLOR_BLACK),
+    16: (curses.COLOR_WHITE, curses.COLOR_BLUE),
+    17: (curses.COLOR_WHITE, curses.COLOR_GREEN),
+    18: (curses.COLOR_WHITE, curses.COLOR_MAGENTA),
+    19: (curses.COLOR_WHITE, curses.COLOR_CYAN),
+    20: (curses.COLOR_WHITE, curses.COLOR_RED)
+}
+
 LOGO = " _____  _____    ___  _____ \n" \
        "/ __  \\|  _  |  /   ||  _  |\n" \
        "`' / /'| |/' | / /| | \\ V / \n" \
        "  / /  |  /| |/ /_| | / _ \\ \n" \
        "./ /___\\ |_/ /\\___  || |_| |\n" \
        "\\_____/ \\___/     |_/\\_____/\n"
+
 LOGO_COLOR = 1
 
 TILE_COLORS = {
-    2: (2, curses.COLOR_WHITE, curses.COLOR_BLUE),
-    4: (3, curses.COLOR_WHITE, curses.COLOR_GREEN),
-    8: (4, curses.COLOR_WHITE, curses.COLOR_MAGENTA),
-    16: (5, curses.COLOR_WHITE, curses.COLOR_CYAN),
-    32: (6, curses.COLOR_WHITE, curses.COLOR_RED),
-    64: (7, curses.COLOR_WHITE, curses.COLOR_YELLOW),
-    128: (8, curses.COLOR_BLACK, curses.COLOR_WHITE),
-    256: (9, curses.COLOR_BLACK, curses.COLOR_BLUE),
-    512: (10, curses.COLOR_BLACK, curses.COLOR_GREEN),
-    1024: (11, curses.COLOR_BLACK, curses.COLOR_MAGENTA),
-    2048: (12, curses.COLOR_BLACK, curses.COLOR_CYAN),
-    4096: (13, curses.COLOR_BLACK, curses.COLOR_RED),
-    8192: (14, curses.COLOR_BLACK, curses.COLOR_YELLOW),
-    16384: (15, curses.COLOR_WHITE, curses.COLOR_BLACK),
-    32768: (16, curses.COLOR_WHITE, curses.COLOR_BLUE),
-    65536: (17, curses.COLOR_WHITE, curses.COLOR_GREEN),
-    131072: (18, curses.COLOR_WHITE, curses.COLOR_MAGENTA),
-    262144: (19, curses.COLOR_WHITE, curses.COLOR_CYAN),
-    524288: (20, curses.COLOR_WHITE, curses.COLOR_RED)
+    2: 2,
+    4: 3,
+    8: 4,
+    16: 5,
+    32: 6,
+    64: 7,
+    128: 8,
+    256: 9,
+    512: 10,
+    1024: 11,
+    2048: 12,
+    4096: 13,
+    8192: 14,
+    16384: 15,
+    32768: 16,
+    65536: 17,
+    131072: 18,
+    262144: 19,
+    524288: 20
 }
-
-LEVELS = (("Master - 3x3", 3, 3),
-          ("Normal - 4x4", 4, 4),
-          ("Easy - 5x5", 5, 5),
-          ("Relax - 6x6", 6, 6))
-
-
-def get_tile_color_pair(tile):
-    return curses.color_pair(TILE_COLORS[524288 if tile >= 524288 else tile][0])
-
-
-def add_cstr(stdscr, y, str):
-    stdscr.addstr(y, 0, str.center(curses.COLS))
 
 
 class NFactorial2048:
@@ -51,7 +70,7 @@ class NFactorial2048:
     def empty_board(cls, height, width):
         return [[0] * width for _ in range(height)]
 
-    def fill_random_empty_tile(self, value):
+    def add_random_tile(self, value):
         empty_tiles = [(row, col)
                        for row, row_tiles in enumerate(self.board)
                        for col, tile in enumerate(row_tiles)
@@ -71,10 +90,11 @@ class NFactorial2048:
             height * 2 + 1, width * 9 + 1,
             (curses.LINES - height * 2 - 1) // 2,
             (curses.COLS - width * 9 - 1) // 2)
+
         self.board = NFactorial2048.empty_board(height, width)
         for _ in range(2):
             # choose 2 with 75% probability and 4 with 25% probability
-            self.fill_random_empty_tile(random.choice([2, 2, 2, 4]))
+            self.add_random_tile(random.choice([2, 2, 2, 4]))
 
         self.state = "PLAYING"
 
@@ -82,13 +102,14 @@ class NFactorial2048:
         self.board_window.clear()
         for i in range(self.width):
             for j in range(self.height):
+                tile = self.board[i][j]
                 self.board_window.addstr(i * 2, j * 9, "+--------")
                 self.board_window.addch(i * 2 + 1, j * 9, "|")
-                if self.board[i][j] != 0:
+                if tile != 0:
                     self.board_window.addstr(
                         i * 2 + 1, j * 9 + 1,
-                        str(self.board[i][j]).center(8),
-                        get_tile_color_pair(self.board[i][j]))
+                        str(tile).center(8),
+                        curses.color_pair(TILE_COLORS.get(tile, 20)))
             self.board_window.addch(i * 2, self.height * 9, "+")
             self.board_window.addch(i * 2 + 1, self.height * 9, "|")
         for j in range(self.height):
@@ -150,8 +171,7 @@ class NFactorial2048:
         elif direction == "RIGHT":
             self.move_right()
 
-        new_tile = random.choice([2] * 6 + [4])
-        self.fill_random_empty_tile(new_tile)
+        self.add_random_tile(random.choice([2] * 6 + [4]))
         self.draw()
 
         won = False
@@ -175,17 +195,24 @@ class NFactorial2048:
                 self.state = "WON AND CANNOT CONTINUE"
 
 
-def game(stdscr, level):
+def adcstr(stdscr, y, str):
+    stdscr.addstr(y, 0, str.center(curses.COLS))
+
+
+def game(stdscr, height, width):
     stdscr.clear()
     stdscr.refresh()
 
-    nfactorial2048 = NFactorial2048(*LEVELS[level][1:])
+    nfactorial2048 = NFactorial2048(height, width)
     nfactorial2048.draw()
 
     continue_game_after_win = False
     while True:
-        add_cstr(stdscr, curses.LINES - 2,
-                 "PRESS Q TO QUIT, ARROW KEYS OR WASD TO MOVE THE TILES")
+        stdscr.move(2, 0)
+        stdscr.clrtoeol()
+        adcstr(stdscr, curses.LINES - 2,
+               "PRESS Q TO QUIT, ARROW KEYS OR WASD TO MOVE THE TILES")
+
         key = stdscr.getkey()
         if key == "KEY_UP" or key.lower() == "w":
             nfactorial2048.move("UP")
@@ -202,9 +229,9 @@ def game(stdscr, level):
 
         if nfactorial2048.state == "LOST" or \
                 nfactorial2048.state == "WON AND CANNOT CONTINUE":
-            add_cstr(stdscr, 2, "GAME OVER!")
-            add_cstr(stdscr, curses.LINES - 2,
-                     "PRESS Q TO QUIT OR ANY OTHER KEY TO RESTART")
+            adcstr(stdscr, 2, "GAME OVER!")
+            adcstr(stdscr, curses.LINES - 2,
+                   "PRESS Q TO QUIT OR ANY OTHER KEY TO RESTART")
             stdscr.refresh()
             curses.napms(1500)
             key = stdscr.getkey()
@@ -212,9 +239,9 @@ def game(stdscr, level):
         elif nfactorial2048.state == "WON AND CAN CONTINUE":
             if continue_game_after_win:
                 continue
-            add_cstr(stdscr, 2, "YOU WON!")
-            add_cstr(stdscr, curses.LINES - 2,
-                     "PRESS Q TO QUIT OR ANY OTHER KEY TO CONTINUE")
+            adcstr(stdscr, 2, "YOU WON!")
+            adcstr(stdscr, curses.LINES - 2,
+                   "PRESS Q TO QUIT OR ANY OTHER KEY TO CONTINUE")
             stdscr.refresh()
             curses.napms(1500)
             key = stdscr.getkey()
@@ -224,45 +251,43 @@ def game(stdscr, level):
                 continue_game_after_win = True
 
 
-def check_window_size():
-    return curses.LINES >= 24 and curses.COLS >= 80
+def logo_show(stdscr):
+    stdscr.move(0, 0)
+    for i in LOGO.splitlines():
+        stdscr.addstr(i.center(curses.COLS),
+                      curses.color_pair(LOGO_COLOR))
 
 
 def main(stdscr):
-    if not check_window_size():
+    if curses.LINES < 24 or curses.COLS < 80:
         raise curses.error("Please resize the terminal to at least 80x24.")
 
     curses.curs_set(0)
 
-    curses.init_pair(LOGO_COLOR, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-
-    for pair in TILE_COLORS.values():
-        curses.init_pair(*pair)
+    for pair, (fg, bg) in COLOR_PAIRS.items():
+        curses.init_pair(pair, fg, bg)
 
     level = 1
     while True:
         stdscr.clear()
-        stdscr.move(0, 0)
-        for tile in LOGO.splitlines():
-            stdscr.addstr(tile.center(curses.COLS),
-                          curses.color_pair(LOGO_COLOR))
-        stdscr.addstr(7, 0, "SELECT DIFFICULTY LEVEL".center(curses.COLS))
-        stdscr.addstr(22, 0, "PRESS Q TO QUIT THE GAME".center(curses.COLS))
+        logo_show(stdscr)
+        adcstr(stdscr, 7, "SELECT DIFFICULTY LEVEL")
+        adcstr(stdscr, 22, "PRESS Q TO QUIT THE GAME")
 
         x = (curses.COLS - 25) // 2
-        for tile, j in enumerate(LEVELS):
-            if tile == level:
+        for i, level_name in enumerate(LEVEL_NAMES):
+            if i == level:
                 attr = curses.A_REVERSE
             else:
                 attr = curses.A_NORMAL
 
-            stdscr.addstr(tile * 3 + 9, x, ("+" + "-" * 23 + "+"), attr)
+            stdscr.addstr(i * 3 + 9, x, ("+" + "-" * 23 + "+"), attr)
             stdscr.addstr(
-                tile * 3 + 10,
+                i * 3 + 10,
                 x,
-                ("|" + j[0].center(23) + "|"),
+                ("|" + level_name.center(23) + "|"),
                 attr)
-            stdscr.addstr(tile * 3 + 11, x, ("+" + "-" * 23 + "+"), attr)
+            stdscr.addstr(i * 3 + 11, x, ("+" + "-" * 23 + "+"), attr)
 
         key = stdscr.getkey()
         if key == "KEY_UP":
@@ -270,7 +295,7 @@ def main(stdscr):
         elif key == "KEY_DOWN":
             level = (level + 1) % len(LEVELS)
         elif key == "\n":
-            continue_game = game(stdscr, level)
+            continue_game = game(stdscr, *LEVELS[level])
             if not continue_game:
                 break
         elif key.lower() == 'q':
